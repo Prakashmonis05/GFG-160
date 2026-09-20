3159bd9d863d11843283cf0b406d689e7f37a8a7
Prakash Monis <Prakashmonis06@gmail.com>
2026-09-20 22:38:51 +0530

    day 3

---
+++ Day 2/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    void pushZerosToEnd(int[] arr) {
+        int pos=0;
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]!=0)
+            {
+                int temp=arr[i];
+                arr[i]=arr[pos];
+                arr[pos]=temp;
+                pos++;
+            }
+        }
+        
+        
+    }
+} 
